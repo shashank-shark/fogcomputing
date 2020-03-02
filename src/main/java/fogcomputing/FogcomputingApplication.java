4fc66524b06ea48ddc3f117d62eda9c4630d68c8
shashank-shark <shashankreddy231@gmail.com>
2020-03-02 22:14:11 +0530

    Initial commit

---
+++ src/main/java/fogcomputing/FogcomputingApplication.java
@@ -0,0 +1,13 @@
+package fogcomputing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogcomputingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogcomputingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fogcomputing/FogcomputingApplicationTests.java
@@ -0,0 +1,13 @@
+package fogcomputing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogcomputingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
