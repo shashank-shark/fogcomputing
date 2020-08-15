963dd3d1f35ae6c608b0c5f8689af1317c9bdcd9
shashank-shark <shashankreddy231@gmail.com>
2020-08-15 23:00:40 +0530

    Added new build

--- src/main/java/fogcomputing/FogcomputingApplication.java
+++ src/main/java/fogcomputing/FogcomputingApplication.java
@@ -3,6 +3,7 @@ package fogcomputing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FogcomputingApplication {
 
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ee88290b130166a257e68f2a4282a22.js"
+  "/precache-manifest.3dd7ab5e79ba723e57fc74aea2226ed3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
